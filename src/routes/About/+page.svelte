f0f8aa4d4de5e62904f56f8fbfe9539b4c9c3987
Bhavya Panchal <panchalbhavya2210@gmail.com>
2024-05-14 23:08:17 +0530

    d

---
+++ src/routes/About/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+  import MyImage from "../../lib/assets/img-self.webp";
+</script>
+
+<main>
+  <div
+    class="relative top-36 sm:top-28 md:top-28 lg:top-28 ml-5 sm:ml-8 md:ml-10 lg:ml-14"
+  >
+    <div class="flex text-3xl">
+      <span class="border-l-4 border-l-main-cyan pl-3">ABOUT ME</span>
+    </div>
+
+    <div class="flex mt-5">
+      <div class="imageContainer">
+        <div
+          class="betaImg flex justify-center items-center w-36 h-36 rounded-full overflow-hidden"
+        >
+          <img src={MyImage} alt="" class="w-40 h-40 object-cover" />
+        </div>
+      </div>
+      <div class="para w-9/12 ml-10 text-xl">
+        Hello, I Am Bhavya. I am a 22-year-old front-end website developer from
+        India. And I am learning the backend. Currently, I am in college, doing
+        the 5th semester of the BCA (Bachelor of Computer Application) Course at
+        Monark University. I started programming as a hobby. I got into
+        programming in lockdown and started learning it from the Mimo app and
+        various YouTube channels. <br />
+        Beside working part-time at a pharmacy and doing college, I created some
+        side projects that gave me a lot of experience. And I am also eager to learn
+        new things. I have learned a lot from practicals.
+        <br /><br />
+        Coding Journey, <br /> I started learning front-end website development
+        in 2020. I did most of the learning from the Mimo App and YouTube
+        Videos. After learning the basics, I got into creating various projects.
+        I created some basic projects and improved them over time. I started
+        coding on my phone, and I created various projects using the SPCK
+        editor. <br /> After coding in mobile for a long time. I bought a laptop
+        and started coding on it. I mostly use Visual Studio Code for programming.
+        I am still learning the basics of the backend so I can become a full-stack
+        developer.
+      </div>
+    </div>
+  </div>
+</main>
