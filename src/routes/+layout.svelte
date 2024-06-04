448497a40d2d24a9c19d67066c8bde1f1225a035
Bhavya Panchal <panchalbhavya2210@gmail.com>
2024-06-04 23:36:13 +0530

    added als

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import "../lib/global.css";
   import Footer from "./Footer.svelte";
   import Header from "./Header.svelte";
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
+  inject();
 </script>
 
 <main>
