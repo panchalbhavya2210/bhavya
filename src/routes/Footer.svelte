e3dfd1f9cb2a05d1ffd8ee1c084f13c0433381e4
Bhavya Panchal <panchalbhavya2210@gmail.com>
2024-09-14 23:38:43 +0530

    new branch

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -5,7 +5,7 @@
 <main>
   <div class="p-5 md:lg-p-11 lg:p-11 relative bottom-10 top-10 text-white">
     <div
-      class="style mx-auto rounded-md grid lg:grid-cols-3 grid-cols-1 gap-10 lg:justify-items-center bg-nav-cyan h-full pb-5 p-5 sm:mt-0 md:mt-0 lg:mt-0 sm:pb-5 md:pb-5 lg:pb-5"
+      class="style mx-auto rounded-lg grid lg:grid-cols-3 grid-cols-1 gap-10 lg:justify-items-center bg-nav-cyan h-full pb-5 p-5 sm:mt-0 md:mt-0 lg:mt-0 sm:pb-5 md:pb-5 lg:pb-5"
     >
       <div class="navigation lg:ml-5 mt-5 ml-5">
         <p class="uppercase text-3xl">Navigation</p>
