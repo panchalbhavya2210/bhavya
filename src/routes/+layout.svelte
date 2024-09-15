4fca4a282f8c4162ce1b3f21de90c1741f5691a9
Bhavya Panchal <panchalbhavya2210@gmail.com>
2024-09-15 18:41:12 +0530

    removal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
   import "../lib/global.css";
   import Footer from "./Footer.svelte";
   import Header from "./Header.svelte";
-  import { dev } from "$app/environment";
   import { inject } from "@vercel/analytics";
 
   inject();
